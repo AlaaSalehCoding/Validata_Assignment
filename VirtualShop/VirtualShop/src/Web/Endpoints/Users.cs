bfcf710198bbb3dece728ba92396f7464c73398f
AlaaSalehCoding <alaasalehcoding@gmail.com>
2025-03-06 06:09:06 +0400

    enable default users

--- VirtualShop/VirtualShop/src/Web/Endpoints/Users.cs
+++ VirtualShop/VirtualShop/src/Web/Endpoints/Users.cs
@@ -4,11 +4,9 @@ namespace VirtualShop.Web.Endpoints;
 public class Users : EndpointGroupBase
 {
     public override void Map(WebApplication app)
-    {
-        ///Commented as customizatio to MapIdentityApi needed to reflect the new attributes
-        ///Other option was to impement the needed apis
-        //app.MapGroup(this)
-        //    .MapIdentityApi<ApplicationUser>();
+    { 
+        app.MapGroup(this)
+            .MapIdentityApi<ApplicationUser>();
 
     }
 }
